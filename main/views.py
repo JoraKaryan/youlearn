6564de1b22d1bce6cd986ea972cffa095a21977d
Zhora <zhora.karyan.01@gmail.com>
2024-12-05 18:01:00 +0400

    counts added to homepage

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from students.models import Student
+from tutors.models import Tutor
+from groups.models import Group
 
 def homepage(request):
-    return render(request, 'main/homepage.html')
+    students_count = Student.objects.count()
+    tutors_count = Tutor.objects.count()
+    groups_count = Group.objects.count()
+    return render(request, 'main/homepage.html', {'students_count': students_count, 'tutors_count': tutors_count, 'groups_count': groups_count})
